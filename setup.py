649bb60b37bbe08c73ec48c8fb359a8c5f60cfc2
Anirudh P M <anirudhparavind@gmail.com>
2023-08-04 00:20:58 +0530

    modification of setup configuration

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 from setuptools import find_packages,setup
+from typing import List
 
+HYPEN_E_DOT = '-e .'
+def get_requirements(filepath:str)->List[str]:
+    """
+    This function will return the list of requirements
+    """
+    requirements = []
+    with open(filepath) as file_obj:
+        requirements = file_obj.readline()
+        requirements = [req.replace("\n","") for req in requirements]
 
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+
+    return requirements
 
 setup(
     name="ML Project",
@@ -8,5 +22,5 @@ setup(
     author='Anirudh-Aravind',
     author_email='anirudhparavind@gmail.com',
     packages=find_packages(),
-    install_requires = ['pandas','numpy','seaborn']    
+    install_requires = get_requirements('requirements.txt')    
 )
